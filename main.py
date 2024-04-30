8a2672efdc61bf22417258d9eb4ca2461526db39
AlyonaPodyapolskaya <135696312+AlyonaPodyapolskaya@users.noreply.github.com>
2024-04-30 16:44:08 +0700

    main.py

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ app.include_router(employees_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8002)
+    uvicorn.run("main:app", host="127.0.0.1", port=8001)
